90f7d9fd765beb350362398c02a6dbf2c2c932a2
darkcobra <darkcobra@TAMY>
2020-11-22 16:57:01 -0600

    SE IMPLEMENTO LA PLANTILLA BOOTSTRAP Y SE INICIO EL DISEÑO DE LOS FORMULARIOS

--- index.php
+++ index.php
@@ -6,13 +6,58 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <title>NetworkFriends - Log In</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="ITVH">
+        <meta name="generator" content="Jekyll v3.8.5">
+        
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="vendor/fontawesome/css/all.css" rel="stylesheet">
     </head>
+    
     <body>
-        <?php
-        // put your code here
-        echo 'hola';
-        ?>
+        
+        <div align="center">
+            <form class="form-horizontal">
+            <fieldset>
+                <br>
+                <br>
+                <br>
+                <br>
+                
+                <!-- Form Name -->
+                <legend></legend>
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="txtUser">User</label>
+                    <div class="col-md-2">
+                        <input id="txtUser" name="txtUser" type="text" placeholder="Enter your username" class="form-control input-md" required="">
+                    </div>
+                </div>
+                
+                <!-- Password input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="txtPassword">Password</label>
+                    <div class="col-md-2">
+                        <input id="txtPassword" name="txtPassword" type="password" placeholder="Enter your password" class="form-control input-md" required="">
+                    </div>
+                </div>
+                
+                <!-- Button (Double) -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="btnLogin"></label>
+                    <div class="col-md-8">
+                        <button id="btnLogin" name="btnLogin" class="btn btn-success">Log In</button>
+                        <button id="btnSignup" name="btnSignup" class="btn btn-inverse">Sign Up</button>
+                    </div>
+                </div>
+                
+            </fieldset>
+        </form>
+        </div>
+        
     </body>
 </html>
